35f4afa6a6e74bd6c7a9432fac720df6de5d6d97
oktydeniz <oktydeniz07@gmail.com>
2021-05-31 11:40:27 +0300

    sql queries

--- deleteUpdateInsert.sql
+++ deleteUpdateInsert.sql
@@ -1,14 +1,14 @@
 
--- Kayıt Ekleme 
--- INSERT INTO shopping.product (idproduct,name,price,imageUrl,category,stock) VALUES('12','Samsung J8','2300','98S.jpg','Telefon','3')
+ Kayıt Ekleme 
+ INSERT INTO shopping.product (idproduct,name,price,imageUrl,category,stock) VALUES('12','Samsung J8','2300','98S.jpg','Telefon','3')
 
 
--- Guncelleme
--- UPDATE shopping.product SET name='Lenovo' where idproduct = 10
--- UPDATE shopping.product SET price=3985 where idproduct = 10;
+ Guncelleme
+ UPDATE shopping.product SET name='Lenovo' where idproduct = 10
+ UPDATE shopping.product SET price=3985 where idproduct = 10;
  
--- eğer koşul belirtmeden bir sorgu yapmak istersek msSQL de bunu yamaya gerek yok 
-/*
+ eğer koşul belirtmeden bir sorgu yapmak istersek msSQL de bunu yamaya gerek yok 
+
 SET SQL_SAFE_UPDATES =0; -- yazılması zorunlu 
 Update  shopping.product set price = price + 1000.12 ;
 select* from shopping.product
@@ -17,11 +17,10 @@ set SQL_SAFE_UPDATES= 0;
 Update shopping.product set price = price*1.1 where category = 'saat';
 
 
-*/
--- image alanı null ise genel bir değer ata  eğer null olmayanlar için ise aynı sorguyu Is NOT Null diye değiştir.
 
--- Update shopping.product set imageUrl = 'temp.jpg' where imageUrl Is Null;
+ image alanı null ise genel bir değer ata  eğer null olmayanlar için ise aynı sorguyu Is NOT Null diye değiştir.
+ Update shopping.product set imageUrl = 'temp.jpg' where imageUrl Is Null;
 
--- Silme 
+ Silme 
 
--- delete from shopping.product where imageUrl='temp.jpg'
+ delete from shopping.product where imageUrl='temp.jpg'
--- groupBy.sql
+++ groupBy.sql
@@ -1,14 +1,14 @@
--- select distinct category from shopping.product kategorileri tekrarlamadan getirir.
+ select distinct category from shopping.product kategorileri tekrarlamadan getirir.
 
--- select category,count(*) as Adet from shopping.product group by category ketegorilerin içinde bulunan urun miktarı 
+ select category,count(*) as Adet from shopping.product group by category ketegorilerin içinde bulunan urun miktarı 
 
--- select category,Sum(price) as toplam from shopping.product group by category
+ select category,Sum(price) as toplam from shopping.product group by category
 
--- select category,avg(price) as ortalama from shopping.product group by category
+ select category,avg(price) as ortalama from shopping.product group by category
 
--- select category,avg(price) as Kosul from shopping.product where price>5000 group by category
+ select category,avg(price) as Kosul from shopping.product where price>5000 group by category
 
--- select category,count(*) as adet from shopping.product group by category having Count(*)>5
+ select category,count(*) as adet from shopping.product group by category having Count(*)>5
 /*
 having ile gruplanmış liste içinde koşul yazarız diğer şekilde yazdığımız koşul ile gruplama sağlanır.
 having ile kriterleri liste boyutunda yapıyoruz. having grubu ilgilendirren koşullar içindir.
